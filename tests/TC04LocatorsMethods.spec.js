cc48c94d92acdfe754aa5beffc0aaa3faa83dc9b
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-07-08 21:34:12 +0530

    LocatorMethod

--- tests/TC04LocatorsMethods.spec.js
+++ tests/TC04LocatorsMethods.spec.js
@@ -16,9 +16,28 @@ test('verify getByAltText method in playwright',async({page})=>{
     expect(ele).toHaveAttribute('alt','letcode')
 })
 
-test.only('Verify getByLabel method in playwright',async({page})=>{
+test('Verify getByLabel method in playwright',async({page})=>{
     await page.goto('https://letcode.in/test#google_vignette')
     let ele2 = page.getByLabel('main navigation')
     await expect(page.getByLabel('main navigation')).toBeVisible()
     await expect(ele2).toHaveAttribute('role','navigation')
+})
+
+test('Verify getByPlaceholder method in playwright',async({page})=>{
+    await page.goto('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    let fstName = await page.getByPlaceholder('First Name')
+    expect(fstName).toBeVisible()
+    expect(fstName).toHaveClass('feedback-input')
+    expect(fstName).toHaveAttribute('name','first_name')
+    fstName.fill('Minskole')
+    await page.waitForTimeout(3000)
+})
+
+test.only('Verify getByRole method in playwright',async({page})=>{
+    await page.goto('https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html')
+    let check1 = await page.getByRole('checkbox',{name:"Option 1"})
+    check1.check()
+    await page.waitForTimeout(3000)
+    expect(check1).toBeVisible()
+    expect(check1).toHaveAttribute('type','checkbox')
 })
