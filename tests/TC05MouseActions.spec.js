4e2090accb11886ee5f605aaf7424921695d5ce3
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-07-18 21:32:12 +0530

    MouseActions

--- tests/TC05MouseActions.spec.js
+++ tests/TC05MouseActions.spec.js
@@ -11,9 +11,21 @@ test('Verify dblclick in playwright',async({page})=>{
     await expect(page.locator('#doubleClickMessage')).toHaveText('You have done a double click')
 })
 
-test.only('Verify rightclick in playwright',async({page})=>{
+test('Verify rightclick in playwright',async({page})=>{
     await page.goto('https://demoqa.com/buttons')
     await page.locator('#rightClickBtn').click({ button: 'right' })
     await expect(page.locator('#rightClickMessage')).toBeVisible()
     await expect(page.locator('#rightClickMessage')).toHaveText('You have done a right click')
+})
+
+test.only('Verify mouseHover in playwright',async({page})=>{
+    await page.goto('https://webdriveruniversity.com/Actions/index.html#')
+    await page.getByText('Hover Over Me Third!').hover()
+    await expect(page.locator('[style="float:right;"] > div > a').first()).toBeVisible()
+    await expect(page.locator('[style="float:right;"] > div > a').last()).toBeVisible()
+    await page.locator('[style="float:right;"] > div > a').nth(1).click()
+    page.on('dialog',async simpleAlert =>{
+        await expect(simpleAlert.message()).toContain('Well done you clicked on the link!')
+        await simpleAlert.accept()
+    })
 })
