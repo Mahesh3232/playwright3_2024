e69ec299682d4e60bbd3fbee567f89a47f232b12
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-07-25 21:26:05 +0530

    Alerts

--- tests/TC07Alerts.spec.js
+++ tests/TC07Alerts.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test')
+const exp = require('constants')
 
 //types of alerts 
 // js simple alert 
@@ -11,15 +12,20 @@ test('Handle js simple in playwright', async ({ page }) => {
         // console.log(simpleAlert.message())
         await expect(simpleAlert.message()).toContain('I am a JS Alert')
         await simpleAlert.accept()
+        await expect(simpleAlert.type()).toContain('alert')
         console.log(simpleAlert.type())
+        //js              Playwright
+        //Simplealert     Alert
+        //confirmAlert    confirm 
+        //PromptAlert     promt
     })
     await page.locator('button[onclick="jsAlert()"]').click()
-
+    await expect(page.locator('#result')).toHaveText('You successfully clicked an alert')
     //await page.waitForTimeout(3000)
 })
 
 
-test.only('Verify simple alert in playwright', async ({ page }) => {
+test('Verify simple alert in playwright', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/')
     await page.waitForSelector('[class="card-title"] > a')
     let productCount = await page.locator('[class="card-title"] > a').count()
@@ -40,3 +46,23 @@ test.only('Verify simple alert in playwright', async ({ page }) => {
     await page.locator('[onclick="addToCart(3)"]').click()
     await page.waitForTimeout(3000)
 })
+
+test.only('Verify confirm in playwright',async({page})=>{
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
+    page.on('dialog', async confirm =>{
+        console.log(confirm.type())
+        await expect(confirm.message()).toContain('I am a JS Confirm')
+        console.log(confirm.message())
+        await expect(confirm.type()).toContain('confirm')
+        //await confirm.accept()
+        await confirm.dismiss()
+    })
+    await page.locator('[onclick="jsConfirm()"]').click()
+    //await expect(page.locator('#result')).toHaveText('You clicked: Ok')
+    await expect(page.locator('#result')).toHaveText('You clicked: Cancel')
+})
+
+
+
+
+
