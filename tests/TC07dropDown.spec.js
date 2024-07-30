fb219e419747cb7eb04e3de9d469e49a70c0da2b
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-07-30 21:35:15 +0530

    alert and dropdown

--- tests/TC07dropDown.spec.js
+++ tests/TC07dropDown.spec.js
@@ -6,8 +6,32 @@ const { test, expect } = require('@playwright/test')
 
 test('Handling static dropdown in playwright',async({page})=>{
     await page.goto('https://letcode.in/dropdowns')
-    await page.locator('#fruits').selectOption('3')
-    let sucessEle = await page.locator('[class="subtitle"]')
-    await expect(sucessEle).toBeVisible()
-    await expect(sucessEle).toHaveText('You have selected Banana')
+    await page.locator('#fruits').selectOption('2')
+    let result =  await page.locator('[class="subtitle"]')
+    expect(result).toBeVisible()
+    expect(result).toHaveText('You have selected Orange')
+})
+
+test('Handling static dropdown using lablel parameter',async({page})=>{
+    await page.goto('https://letcode.in/dropdowns')
+    await page.locator('#fruits').selectOption({ label: 'Orange' })
+    let result =  await page.locator('[class="subtitle"]')
+    expect(result).toBeVisible()
+    expect(result).toHaveText('You have selected Orange')
+})
+
+test('Handling static dropdown 3',async({page})=>{
+    await page.goto('https://letcode.in/dropdowns')
+    await page.locator('#superheros').selectOption('ca')
+    let result2 = await page.locator('[class="subtitle"]')
+    expect(result2).toBeVisible()
+    expect(result2).toHaveText('You have selected Captain America') 
+})
+
+test.only('Handling static dropdown 4',async({page})=>{
+    await page.goto('https://letcode.in/dropdowns')
+    await page.locator('#lang').selectOption({label:"Swift"})
+    let result3 = await page.locator('[class="subtitle"]')
+    expect(result3).toBeVisible()
+    expect(result3).toHaveText('You have selected Swift') 
 })
