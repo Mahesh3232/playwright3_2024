ec59ab40439c76cc3a2b6ed051f3c3cc8adeba20
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-08-19 08:26:51 +0530

    Fileuplode

--- tests/TC11Fileuplode.spec.js
+++ tests/TC11Fileuplode.spec.js
@@ -12,3 +12,16 @@ test('Verify fileuplode in playwright',async({page})=>{
     await expect(page.url())
     .toContain('https://webdriveruniversity.com/File-Upload/index.html?filename=FileUplode+Demo+File+1.pdf')
 })
+
+test.only('Verify multiple file uplode in playwright',async({page})=>{
+    await page.goto('https://davidwalsh.name/demo/multiple-file-upload.php')
+    let file1 = 'tests/UplodeData/FileUplode Demo File 1.pdf'
+    let file2 = 'tests/UplodeData/FileUplode Demo File 2.pdf'
+    await page.locator('#filesToUpload').setInputFiles([file1,file2])
+    let filecount = await page.locator('#fileList>li').count()
+    expect(filecount).toBe(2)
+    expect(await page.locator('#fileList>li').first()).toHaveText('FileUplode Demo File 1.pdf')
+    expect(await page.locator('#fileList>li').last()).toHaveText('FileUplode Demo File 2.pdf')
+    console.log(filecount)
+    await page.waitForTimeout(3000)
+})
