3bfa97589136e996e043b3dee61106fc0e463d1b
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-09-05 08:36:18 +0530

    MultiTab

--- tests/TC16MultiTab.spec.js
+++ tests/TC16MultiTab.spec.js
@@ -11,3 +11,40 @@ test('Validate multitab in playwright',async({browser})=>{
     await newpage.waitForTimeout(4000)
     await expect(newpage.locator('[alt="Logo"]').first()).toBeVisible()
 })
+
+test('Verify multiTab with removing target _blank attribute',async({browser})=>{
+    const context = await browser.newContext() //launches New fresh browser
+    const page = await context.newPage()//launches New fresh tab in browser
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    await page.evaluate(()=>{
+        const selector = document.querySelector('[id="opentab"]')
+        selector.removeAttribute('target','_blank')
+    })
+    await page.locator('[href="https://www.qaclickacademy.com"]').click()
+    await page.waitForTimeout(5000)
+})
+
+test('Verify multiTab with remove and add attribute',async({browser})=>{
+    const context = await browser.newContext() //launches New fresh browser
+    const page = await context.newPage()//launches New fresh tab in browser
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    await page.evaluate(()=>{
+        const selector = document.querySelector('[id="opentab"]')
+        selector.removeAttribute('target','_blank')
+        selector.setAttribute('target','_Self')
+    })
+    await page.locator('[href="https://www.qaclickacademy.com"]').click()
+    await page.waitForTimeout(5000)
+})
+
+test.only('Add disabled attribute to input element ',async({browser})=>{
+    const context = await browser.newContext() //launches New fresh browser
+    const page = await context.newPage()//launches New fresh tab in browser
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    await page.evaluate(()=>{
+        const selector = document.querySelector('input[value="radio1"]')
+        selector.setAttribute('action','disabled')
+    })
+    await page.waitForTimeout(5000)
+})
+
