939d22fa27d7690af3b96981847b2955c8b75b8a
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-10-07 08:40:46 +0530

    WebTable

--- tests/TC19webTable.spec.js
+++ tests/TC19webTable.spec.js
@@ -20,4 +20,28 @@ test('Handling webTable in playwright',async({page})=>{
 
     let rowsCount = await table.locator('tbody > tr').count()
     console.log(`Rows count = ${rowsCount}`)
+
+    //Select checkbox for singal Tablet 
+     let rows = await table.locator('tbody > tr')
+    // let matchRow = rows.filter({
+    //     has:page.locator('td'),
+    //     hasText:'Tablet '
+    // })
+    // await matchRow.locator('input').check()
+    // await page.waitForTimeout(5000)
+
+    //select checkbox for multiple products 
+    await checkProducts(rows,page,'Laptop')
+    await checkProducts(rows,page,'Smartwatch')
+    await checkProducts(rows,page,'Wireless Earbuds')
+    await page.waitForTimeout(5000)
 })
+
+async function checkProducts(row,page,productName){
+    let matchRow = row.filter({
+        has:page.locator('td'),
+        hasText:productName
+    })
+    await matchRow.locator('input').check()
+}
+
